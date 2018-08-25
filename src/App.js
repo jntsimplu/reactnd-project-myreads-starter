3e9f70951283c1935e7acf5734b116239b02fa39
Boltea Cosmin-Robert <huliganspliff@gmail.com>
2018-08-26 00:16:30 +0300

    Set the MainPage books automatically

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 import MainPage from './MainPage.js'
 import Book from './Book.js'
@@ -7,19 +7,21 @@ import SearchPage from './SearchPage.js'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
-    showSearchPage: false
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books: books })
+    })
   }
 
   render() {
     return (
       <div className="app">
-        <MainPage />
+        <MainPage
+        books={this.state.books}
+        />
       </div>
     )
   }
